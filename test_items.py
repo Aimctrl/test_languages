108368910d9ca33d08d697c27692de16877125d8
Fatum <73196082+Aimctrl@users.noreply.github.com>
2025-10-17 18:04:56 +0700

    Update test_items.py
    Изменение времени задержки (до 60 секунд)

--- test_items.py
+++ test_items.py
@@ -4,7 +4,8 @@ from selenium.webdriver.common.by import By
 def test_localization_link(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(3)
+    time.sleep(60)
 
     assert browser.find_element(By.CSS_SELECTOR, '[class*="btn-add-to-basket"]'), \
+
         "Кнопка добавления товара в корзину не найдена"
